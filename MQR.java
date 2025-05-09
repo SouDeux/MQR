fdc96e5a99116ad54ed59f675b7b3920cff5a1ca
Soul_Deux <bruno1m999k@gmail.com>
2025-05-09 06:45:29 -0300

    Atualizar o MQRMobile.java

--- MQR.java
+++ MQR.java
@@ -14,9 +14,9 @@ public class MQR {
     boolean jogardnv;
 
 
-        System.out.println("------------------------------------------------------------------------------");
-        System.out.println("                    Bem vindo ao MQR (Melhor Que o Rollem)!                  ");
-        System.out.println("------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("             Bem vindo ao MQR (Melhor Que o Rollem)!              ");
+        System.out.println("-------------------------------------------------------------------");
 
     do {
         boolean mult;
@@ -26,14 +26,14 @@ public class MQR {
         double media;
 
         // inicio
-        System.out.println("------------------------------------------------------------------------------");
-        System.out.println("                                Boa sorte! :)                                 ");
-        System.out.println("------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("                            Boa sorte! :)                         ");
+        System.out.println("-------------------------------------------------------------------");
 
 
         // checa se o usuário irá jogar multiplos dados, estando dentro de um loop para o caso de ele digitar errado:
         while (true){
-            System.out.println("pretende jogar mais de um dado? (s/n):");
+            System.out.print("  Pretende jogar mais de um dado? (s/n): ");
             String skii = s.next();
             if (skii.equalsIgnoreCase("n")){
                 mult = false;
@@ -44,17 +44,17 @@ public class MQR {
                 break;
             }
             else{
-                System.out.println("apenas s ou n! ");
+                System.out.println("  Apenas s ou n! ");
             }
         }
 
 
         // checa a quantidade de dados desejados
         if (mult) {
-            System.out.println("Digite a quantidade de dados que deseja: ");
+            System.out.print("  Digite a quantidade de dados que deseja: ");
             qntdados = s.nextInt();
             if (qntdados <= 1) {  // checa se o jogador vai jogar um único dado, transformando mult em false
-                System.out.println("nesse caso, será somente um dado.");
+                System.out.println("  Nesse caso, será somente um dado.");
                 mult = false;
             }
         }
@@ -64,64 +64,67 @@ public class MQR {
 
         // checa a quantidade de lados e joga os dados, estando dentro de um loop para caso o jogador digite um número estranho de lados
         while (true){
-            System.out.println("Digite a quantidade de lados que deseja: ");
+            System.out.print("  Digite a quantidade de lados que deseja: ");
             int lados = s.nextInt(); // pega a quantidade de lados do(s) dado(s)
             if (lados > 1) {  // checa se tem mais de 0 lados
-                    System.out.println("------------------------------------------------------------------------------");
-                    System.out.print("                                Resultado");
+                    System.out.println("-------------------------------------------------------------------");
+                    System.out.print("                             Resultado");
                     if (mult){
                         System.out.println("s!!!                               ");
                     }
                     else{
                         System.out.println("!!!                                ");
                     }
-                    System.out.println("------------------------------------------------------------------------------");
+                    System.out.println("-------------------------------------------------------------------");
 
                 if (mult){
-                    System.out.print("os valores sorteados foram: ");
+                    System.out.print("  Os valores sorteados foram: ");
                 }
                 else{
-                    System.out.print("o valor sorteado foi: ");
+                    System.out.print("  O valor sorteado foi: ");
                 }
 
                 for (int iter = 0; iter < qntdados; iter++){
-                    valor = r.nextInt(1, (lados + 1));
-                    System.out.print(valor + ", ");
+                    valor = r.nextInt(lados) + 1;
+                    System.out.print(valor);
+		    if (iter < qntdados - 1) {
+      			System.out.print(", ");
+  		    }
                     soma += valor;
                 }
 
                 double maximo = lados * qntdados;
                 media = soma / maximo;
                 if (mult){
-                    System.out.print("com a soma: " + soma + ", sendo a media: " + ( (double)soma / qntdados) + ". ");
+                    System.out.print("\n\n —— Soma: " + soma + "\n\n —— Média: " + ( (double)soma / qntdados) + ". \n\n —— ");
                 }
 
 
                 if (media <= 1. / 6 || soma == 1) {
-                    System.out.println("Que paia, é pra ser... :'(");
+                    System.out.println("  Que paia, é pra ser... :'(");
                 } else if (media <= 2. / 6) {
-                    System.out.println("Não é um dado bom.");
+                    System.out.println("  Não é um dado bom.");
                 } else if (media < 3. / 6) {
-                    System.out.println("Podia ser pior, né?");
+                    System.out.println("  Podia ser pior, né?");
                 } else if (media == 3. / 6) {
-                    System.out.println("Tá na média ¯\\_(ツ)_/¯");
+                    System.out.println("  Tá na média ¯\\_(ツ)_/¯");
                 } else if (media <= 4. / 6) {
-                    System.out.println("Que bom!");
+                    System.out.println("  Que bom!");
                 } else if (media < 6. / 6) {
-                    System.out.println("VAMOOOO!");
+                    System.out.println("  VAMOOOO!");
                 } else if (media / qntdados == 1) {
-                    System.out.println("É pra ser :D");
+                    System.out.println("  É pra ser :D");
                 } else {
-                    System.out.println("???");
+                    System.out.println("  ???");
                 }
                 break;
 
             }
             else if (lados == 1){
-                System.out.println("sempre vai cair 1, né? ¯\\_(ツ)_/¯");
+                System.out.println("  sempre vai cair 1, né? ¯\\_(ツ)_/¯");
             }
             else{
-                System.out.println("não pode ser menor que 1.");
+                System.out.println("  não pode ser menor que 1.");
             }
         }
 
@@ -130,10 +133,10 @@ public class MQR {
         // pra checar se o jogador vai jogar dnv
 
         while (true){
-            System.out.println("deseja jogar novamente? (s/n)");
+            System.out.print("  deseja jogar novamente? (s/n): ");
             String m = s.next();
             if (m.equalsIgnoreCase("n")){
-                System.out.println("obrigado por jogar, volte sempre!");
+                System.out.println("  obrigado por jogar, volte sempre!");
                 jogardnv = false;
                 break;
             }
@@ -142,7 +145,7 @@ public class MQR {
                 break;
             }
             else{
-                System.out.println("apenas s ou n!");
+                System.out.println("  apenas s ou n!");
             }
         }
 
